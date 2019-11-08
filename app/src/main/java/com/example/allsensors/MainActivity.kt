def2b16c4479136b3d653dbd046329d6b05c449b
Adriano Ney Nascimento do Amaral <adriano.amaral147@gmail.com>
2019-11-08 09:15:31 -0300

    mostrando todos os sensores do dispositivo em uma lista

--- app/src/main/java/com/example/allsensors/MainActivity.kt
+++ app/src/main/java/com/example/allsensors/MainActivity.kt
@@ -2,25 +2,28 @@ package com.example.allsensors
 
 import android.content.Context
 import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
-import java.io.IOException
+
+import android.widget.ArrayAdapter
+import android.widget.ListView
+//import android.widget.Toast
 
 class MainActivity : AppCompatActivity(){
 
     private lateinit var sensorManager: SensorManager
-    private var sensor: Sensor? = null
+    private lateinit var lista: ArrayList<Sensor>
+    private lateinit var lvSensor: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.content_main)
+
+        this.lista = arrayListOf()
+        this.lvSensor = findViewById(R.id.lvListSensor)
+        this.lvSensor.adapter = ArrayAdapter<Sensor>(this, android.R.layout.simple_list_item_1, this.lista)
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val deviceSensors: List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL)
@@ -28,11 +31,17 @@ class MainActivity : AppCompatActivity(){
 
         while (iterator.hasNext()){
             val sensor = iterator.next()
+            this.lista.add(sensor)
+            this.atualizaLista()
             Log.i("APP_SENSORS", "Name: ${sensor.name}")
             Log.i("APP_SENSORS", "ToString: ${sensor.toString()}")
             Log.i("APP_SENSORS", "-- OUTRO --")
-            Toast.makeText(this@MainActivity, sensor.name.toString(), Toast.LENGTH_LONG).show()
+//            Toast.makeText(this@MainActivity, sensor.name.toString(), Toast.LENGTH_LONG).show()
         }
+    }
+
+    fun atualizaLista(){
+        (this.lvSensor.adapter as ArrayAdapter<Sensor>).notifyDataSetChanged()
     }
 
 }
